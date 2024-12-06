ca7b5e310dabc6156deca4c5fa3f069f7cc0df5c
Thomas BOISSON <thomas.boisson@epita.fr>
2024-12-06 15:26:22 +0100

    projet final

--- src/websocketServer.js
+++ src/websocketServer.js
@@ -1,18 +1,16 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 4242 }); // Serveur WebSocket
+const wss = new WebSocket.Server({ port: 4242 }); 
 
-// Gestion des nouvelles connexions
 wss.on('connection', (ws) => {
     console.log('Client connecté au WebSocket.');
     ws.send(JSON.stringify({ message: 'Bienvenue sur le WebSocket' }));
 });
 
-// Fonction pour diffuser les événements à tous les clients connectés
 function broadcastEvent(event) {
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(event)); // Envoie de l'événement au client
+            client.send(JSON.stringify(event));
         }
     });
 }
