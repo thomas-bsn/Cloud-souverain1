ee555546a85bf7e2f71b151171c6cedd87216006
Thomas BOISSON <thomas.boisson@epita.fr>
2024-12-06 16:36:00 +0100

    fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Nom du conteneur
+CONTAINER_NAME="cloud-souverain-container"
+
+# Vérifier si le conteneur existe déjà
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    echo "Un conteneur existant avec le nom $CONTAINER_NAME a été trouvé. Suppression en cours..."
+    docker rm -f $CONTAINER_NAME
+    if [ $? -eq 0 ]; then
+        echo "Conteneur $CONTAINER_NAME supprimé avec succès."
+    else
+        echo "Erreur lors de la suppression du conteneur $CONTAINER_NAME."
+        exit 1
+    fi
+fi
+
 # Construire l'image Docker
 echo "Construction de l'image Docker..."
 docker build -t cloud-souverain .
@@ -14,12 +29,13 @@ fi
 
 # Lancer le conteneur Docker
 echo "Lancement du conteneur Docker..."
-docker run -p 4242:4242 -p 3000:3000 --name cloud-souverain-container -d cloud-souverain
+docker run -p 4242:4242 -p 3000:3000 --name $CONTAINER_NAME -d cloud-souverain
 
 # Vérifier si le conteneur s'est lancé avec succès
 if [ $? -eq 0 ]; then
     echo "Conteneur Docker lancé avec succès."
     echo "Frontend accessible sur http://localhost:3000"
+    echo "Backend accessible sur http://localhost:4242"
 else
     echo "Erreur lors du lancement du conteneur Docker."
     exit 1
